e775e7ef0597ab3fb2f279cb524f12d7dfc24fae
Mikael <mimikk21@student.aau.dk>
2023-03-16 09:35:15 +0100

    sql file indentation fix

--- app/routers/sql/distributed_table_size.sql
+++ app/routers/sql/distributed_table_size.sql
@@ -7,25 +7,25 @@ WITH RECURSIVE pg_inherit(inhrelid, inhparent) AS
     WHERE child.inhparent = parent.inhrelid),
     pg_inherit_short AS (SELECT * FROM pg_inherit WHERE inhparent NOT IN (SELECT inhrelid FROM pg_inherit))
     SELECT TABLE_NAME,
-	   relation_size AS CITUS_RELATION_SIZE,
-	   table_size AS CITUS_TABLE_SIZE,
-	   total_relation_size AS CITUS_TOTAL_RELATION_SIZE
+       relation_size AS CITUS_RELATION_SIZE,
+       table_size AS CITUS_TABLE_SIZE,
+       total_relation_size AS CITUS_TOTAL_RELATION_SIZE
     FROM (
         SELECT *
     FROM (
          SELECT c.oid,
-				relname AS TABLE_NAME,
-				SUM(citus_relation_size(c.oid)) OVER (partition BY parent) as relation_size,
-				SUM(citus_table_size(c.oid)) OVER (partition BY parent) AS table_size,
-				SUM(citus_total_relation_size(c.oid)) OVER (partition BY parent) AS total_relation_size,
-				parent
+    			relname AS TABLE_NAME,
+    			SUM(citus_relation_size(c.oid)) OVER (partition BY parent) as relation_size,
+    			SUM(citus_table_size(c.oid)) OVER (partition BY parent) AS table_size,
+    			SUM(citus_total_relation_size(c.oid)) OVER (partition BY parent) AS total_relation_size,
+    			parent
           FROM (
                 SELECT pg_class.oid,
-			           reltuples,
-			  		   relname,
-			  		   relnamespace,
-			  		   pg_class.reltoastrelid,
-			           COALESCE(inhparent, pg_class.oid) parent
+    		           reltuples,
+    		  		   relname,
+    		  		   relnamespace,
+    		  		   pg_class.reltoastrelid,
+    		           COALESCE(inhparent, pg_class.oid) parent
                 FROM pg_class
                     LEFT JOIN pg_inherit_short ON inhrelid = oid
                 WHERE relkind IN ('r', 'p') AND (relname LIKE ('fact_%') OR relname LIKE ('dim_%') OR relname = 'danish_waters')
