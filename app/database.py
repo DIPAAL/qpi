cccfa933538b0a43745883d080f1ac5a3a46069d
Lau Josefsen <ljosef18@student.aau.dk>
2023-03-28 23:34:55 +0200

    Now use sqlite when testing

--- app/database.py
+++ app/database.py
@@ -1,4 +1,6 @@
 """Connect to the database and declare a sessionmaker."""
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
@@ -8,6 +10,9 @@ config = get_config()
 
 SQLALCHEMY_DATABASE_URL = \
     f"postgresql://{config['Database']['user']}@{config['Database']['host']}/{config['Database']['database']}"
+
+if os.getenv("IS_TESTING", False):
+    SQLALCHEMY_DATABASE_URL = f"sqlite:///{config['Database']['database']}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={}
