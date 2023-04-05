cbf1124ee6f5f9eef9e07b33226b3a7f668a1294
Mikael <mimikk21@student.aau.dk>
2023-04-05 19:59:36 +0200

    Removed database.py, datawarehouse.py served same purpose

--- app/datawarehouse.py
+++ app/datawarehouse.py
@@ -1,7 +1,8 @@
-"""Data warehouse connection."""
+"""Connect to the data warehouse connection and declare a sessionmaker."""
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 from helper_functions import get_config
 
 config = get_config()
@@ -10,9 +11,13 @@ password = config['Database']['password']
 server = config['Database']['host']
 database = config['Database']['database']
 
-DW_URL = f"postgresql://{user}:{password}@{server}/{database}"
 
-engine = create_engine(DW_URL)
+if os.getenv("IS_TESTING", False):
+    SQLALCHEMY_DATABASE_URL = f"sqlite:///{database}.sqlite"
+else:
+    SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{server}/{database}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
