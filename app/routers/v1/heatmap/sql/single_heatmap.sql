5c4762ef961107b885e85e975b27dda72497522f
Lau Josefsen <ljosef18@student.aau.dk>
2023-04-10 14:04:45 +0200

    Added support for the new heatmaps

--- app/routers/v1/heatmap/sql/single_heatmap.sql
+++ app/routers/v1/heatmap/sql/single_heatmap.sql
@@ -15,21 +15,31 @@ FROM (
     SELECT ST_MapAlgebra(q2.rast, reference.rast, '[rast1.val]+[rast2.val]', extenttype := 'SECOND') AS rast
     FROM reference, (
         SELECT ST_Union(q1.rast) AS rast FROM (
-            SELECT ST_Union(fch.rast, 'SUM') AS rast
-            FROM fact_cell_heatmap fch
-            JOIN dim_ship_type dst on fch.ship_type_id = dst.ship_type_id
-            WHERE fch.spatial_resolution = :spatial_resolution
-            AND fch.heatmap_type_id = (SELECT heatmap_type_id FROM dim_heatmap_type WHERE slug = :heatmap_type_slug)
-            AND timestamp_from_date_time_id(fch.date_id, fch.time_id) <= :end_timestamp
-            AND timestamp_from_date_time_id(fch.date_id, fch.time_id) >= :start_timestamp
-            AND dst.ship_type = ANY (:ship_types)
-            AND dst.mobile_type = ANY (:mobile_types)
-            AND fch.cell_x >= :min_cell_x
-            AND fch.cell_x < :max_cell_x
-            AND fch.cell_y >= :min_cell_y
-            AND fch.cell_y < :max_cell_y
-            AND fch.date_id BETWEEN :start_date_id AND :end_date_id
-            GROUP BY fch.partition_id
+            SELECT
+                -- If there are 2 bands in the raster, assume it is to calculate average by dividing the first band by the second band
+                CASE WHEN ST_Numbands(q0.rast) > 1 THEN
+                    ST_MapAlgebra(q0.rast, 1, q0.rast, 2, '[rast1.val]/[rast2.val]', extenttype := 'FIRST')
+                ELSE
+                    q0.rast
+                END AS rast
+            FROM (
+                SELECT
+                    fch.partition_id, ST_Union(fch.rast, (SELECT union_type FROM dim_heatmap_type WHERE slug = :heatmap_type_slug)) AS rast
+                FROM fact_cell_heatmap fch
+                JOIN dim_ship_type dst on fch.ship_type_id = dst.ship_type_id
+                WHERE fch.spatial_resolution = :spatial_resolution
+                AND fch.heatmap_type_id = (SELECT heatmap_type_id FROM dim_heatmap_type WHERE slug = :heatmap_type_slug)
+                AND timestamp_from_date_time_id(fch.date_id, fch.time_id) <= :end_timestamp
+                AND timestamp_from_date_time_id(fch.date_id, fch.time_id) >= :start_timestamp
+                AND dst.ship_type = ANY (:ship_types)
+                AND dst.mobile_type = ANY (:mobile_types)
+                AND fch.cell_x >= :min_cell_x
+                AND fch.cell_x < :max_cell_x
+                AND fch.cell_y >= :min_cell_y
+                AND fch.cell_y < :max_cell_y
+                AND fch.date_id BETWEEN :start_date_id AND :end_date_id
+                GROUP BY fch.partition_id
+            ) q0
         ) q1
     ) q2
 )q3
