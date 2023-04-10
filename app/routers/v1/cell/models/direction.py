bd3ac802fd56d2f0a8fd0ee0a4cd8d79be8d8b76
Wolf <aklitg13@student.aau.dk>
2023-04-10 12:50:52 +0200

    Updated models to schemas and described their fields for documentation

--- app/routers/v1/cell/models/direction.py
+++
@@ -1,9 +0,0 @@
-"""Model representing a direction in the DIPAAL data warehouse."""
-from pydantic import BaseModel
-
-
-class Direction(BaseModel):
-    """Direction model."""
-
-    begin: str
-    end: str
---
+++ app/routers/v1/cell/schemas/direction.py
@@ -0,0 +1,9 @@
+"""Model representing a direction in the DIPAAL data warehouse."""
+from pydantic import BaseModel, Field
+
+
+class Direction(BaseModel):
+    """Direction model."""
+
+    begin: str = Field(description='The coordinal direction where the ship entered the cell, unknown indicates the ship started in the cell')
+    end: str = Field(description='The coordinal direction where the ship exited the cell, unknown indicates the ship ended in the cell')
