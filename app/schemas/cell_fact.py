168a212b8637a4455fee64920a9174f6d50f52f9
Wolf <aklitg13@student.aau.dk>
2023-04-12 12:42:53 +0200

    Removed ignoring of flake8 line length

--- app/schemas/cell_fact.py
+++ app/schemas/cell_fact.py
@@ -15,9 +15,11 @@ class FactCell(BaseModel):
     entry_timestamp: datetime = Field(description='Timestamp that specifies when the ship entered the cell')
     exit_timestamp: datetime = Field(description='Timestamp that specifies when the ship left the cell')
     navigational_status: str = Field(description='The navigational status of the ship during its stay in the cell')
-    direction: Direction = Field(description='Describes the coordinal direction which the ship entered and exited the cell')  # noqa: E501
+    direction: Direction = Field(description='Describes the coordinal direction'
+                                 ' which the ship entered and exited the cell')
     sog: float = Field(description='The speed over ground of the ship while within the cell')
-    delta_cog: float = Field(description='Describes how much the course over ground of the ship changed while within the cell')  # noqa: E501
+    delta_cog: float = Field(description='Describes how much the course over ground'
+                             ' of the ship changed while within the cell')
     delta_heading: float = Field(description='Describes how much the heading of the ship changed while within the cell')
     draught: float | None = Field(description='The maximum draught set by the ship while within the cell')
     stopped: bool = Field(description='Indicated whether the ship has been inferred to be stopped within the cell')
