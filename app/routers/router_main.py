5906142abded4f46bee01f9bb0c4d625487a932f
Mikael <mimikk21@student.aau.dk>
2023-04-17 14:27:31 +0200

    Should have restored and fixed audit and cell endpoints

--- app/routers/router_main.py
+++ app/routers/router_main.py
@@ -5,7 +5,9 @@ Collects all routers from submodules into a single router for easier import in a
 """
 from fastapi import APIRouter
 from app.routers.v1 import basic_sql, health
+from app.routers.v1.audit_log import audit_log
 from app.routers.v1.heatmap import heatmap
+from app.routers.v1.cell import cell
 from app.routers.v1.ship import router as ship
 
 # Routers for different versions of the API can be added here
@@ -15,6 +17,8 @@ router_v1.include_router(basic_sql.router, prefix="/table", tags=["basic_sql"])
 router_v1.include_router(health.router, prefix="/health", tags=["health"])
 router_v1.include_router(ship.router, prefix="/ships", tags=["ships"])
 router_v1.include_router(heatmap.router, prefix="/heatmap", tags=["heatmap"])
+router_v1.include_router(cell.router, prefix='/cells', tags=['cell'])
+router_v1.include_router(audit_log.router, prefix="/audit_log", tags=["audit_log"])
 
 # The main router for the API app. This router is imported in api_main
 router_main = APIRouter()
