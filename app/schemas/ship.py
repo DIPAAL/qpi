bd982d0c754d4c10f4519ba3e5ef63fc26fecaf1
Mikael <mimikk21@student.aau.dk>
2023-05-12 09:48:51 +0200

    Added default values to the ship schema

--- app/schemas/ship.py
+++ app/schemas/ship.py
@@ -6,17 +6,17 @@ from app.schemas.ship_type import ShipType
 class Ship(BaseModel):
     """Ship Model."""
 
-    ship_id: int
-    name: str
-    callsign: str
-    mmsi: int
-    imo: int
-    flag_region: str
-    flag_state: str
-    mobile_type: MobileType
-    ship_type: ShipType
-    location_system_type: str
-    a: int
-    b: int
-    c: int
-    d: int
+    ship_id: int = 1
+    name: str = "DANPILOT BRAVO"
+    callsign: str = "OXKI2"
+    mmsi: int = 219022257
+    imo: int = 9812092
+    flag_region: str = 219
+    flag_state: str = "Denmark"
+    mobile_type: MobileType = MobileType.class_a
+    ship_type: ShipType = ShipType.pilot
+    location_system_type: str = "AIS"
+    a: int = 17
+    b: int = 3
+    c: int = 2
+    d: int = 4
