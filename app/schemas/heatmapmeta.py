91d9fe5c6d5188d4d1c31c03691a1483bd25c810
Mikael <mimikk21@student.aau.dk>
2023-05-15 15:05:10 +0200

    Added example for heatmap metadata endpoint

---
+++ app/schemas/heatmapmeta.py
@@ -0,0 +1,29 @@
+"""Model for portraying the metadata of a heatmap."""
+from pydantic import BaseModel, Field
+from datetime import datetime
+
+class TemporalDomain(BaseModel):
+    """Model for portraying the temporal domain of a heatmap."""
+    start: datetime
+    end: datetime
+
+
+class TemporalResolution(BaseModel):
+    """Model for portraying the resolution of a heatmap."""
+    resolution: int
+    units: str
+    temporal_domain: TemporalDomain
+
+
+class CellSize(BaseModel):
+    """Model for portraying the cell size of a heatmap."""
+    resolution: int
+    units: str
+    temporal_resolutions: dict[str, TemporalResolution]
+
+
+class HeatmapMetadata(BaseModel):
+    """Model for portraying the metadata of a heatmap."""
+    name: str
+    description: str
+    spatial_resolutions: dict[str, CellSize]
