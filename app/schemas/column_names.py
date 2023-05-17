3a688e9e42876ef16a3e83c0ce7481d7d2aa1ac0
Mikael <mimikk21@student.aau.dk>
2023-05-17 13:19:01 +0200

    flake8

--- app/schemas/column_names.py
+++ app/schemas/column_names.py
@@ -1,6 +1,7 @@
 """Pydantic model for portraying the column names of a relation."""""
 from pydantic import BaseModel, Field
 
+
 class ColumnNames(BaseModel):
     """Pydantic model for portraying the column names of a relation."""
 
