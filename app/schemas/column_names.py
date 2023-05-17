1d3de93c3e88278fd8dd5580cc7affb7401f2f37
Mikael <mimikk21@student.aau.dk>
2023-05-17 13:07:35 +0200

    Split model into 2

---
+++ app/schemas/column_names.py
@@ -0,0 +1,8 @@
+"""Pydantic model for portraying the column names of a relation."""""
+from pydantic import BaseModel, Field
+
+class ColumnNames(BaseModel):
+    """Pydantic model for portraying the column names of a relation."""
+
+    columns: list[str] = Field(description='The column names of the table',
+                               default=["time", "time_id", "hour_of_day", "minute_of_hour", "second_of_minute"])
