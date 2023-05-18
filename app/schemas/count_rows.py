d14a19bd5398f62b6db2b8ecbda6b36ac1ac27eb
Mikael <mimikk21@student.aau.dk>
2023-05-18 11:10:00 +0200

    All these . makes a circle

--- app/schemas/count_rows.py
+++ app/schemas/count_rows.py
@@ -5,4 +5,4 @@ from pydantic import BaseModel, Field
 class CountRows(BaseModel):
     """Pydantic model for portraying the number of rows in a relation."""
 
-    count: int = Field(description='The number of rows in the relation')
+    count: int = Field(description='The number of rows in the relation.')
